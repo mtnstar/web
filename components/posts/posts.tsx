868570ad541bec230ef16af6b301ad573006e984
Mountain Star <github@mtnstar.net>
2022-12-09 22:15:23 +1300

    Add date to posts list

--- components/posts/posts.tsx
+++ components/posts/posts.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { BsArrowRight } from "react-icons/bs";
 import { useTheme } from "../layout";
+import format from "date-fns/format";
 
 export const Posts = ({ data }) => {
   const theme = useTheme();
@@ -17,10 +18,21 @@ export const Posts = ({ data }) => {
     yellow: "group-hover:text-yellow-500 dark:group-hover:text-yellow-300",
   };
 
+  const formattedDate = (post) => {
+    let formattedDate = "";
+    const date = new Date(post._values.date);
+    if (!isNaN(date.getTime())) {
+      formattedDate = format(date, "MMM dd, yyyy");
+    }
+    return formattedDate;
+  }
+
   return (
     <>
       {data.map((postData) => {
+        console.log(postData);
         const post = postData.node;
+      
         return (
           <Link
             key={post._sys.filename}
@@ -31,6 +43,12 @@ export const Posts = ({ data }) => {
               key={post.id}
               className="group block px-8 py-10 mb-8 last:mb-0 bg-gray-50 bg-gradient-to-br from-gray-50 to-gray-100 dark:bg-gray-700 dark:from-gray-800 dark:to-gray-700 rounded-md shadow-sm transition-all duration-150 ease-out hover:shadow-md hover:to-gray-50 dark:hover:to-gray-600"
             >
+              <p
+                data-tinafield="date"
+                className="text-base text-gray-400 group-hover:text-gray-500 dark:text-gray-300 dark:group-hover:text-gray-150"
+              >
+                {formattedDate(post)}
+              </p>
               <h3
                 className={`text-gray-900 dark:text-white text-3xl font-semibold title-font mb-5 transition-all duration-150 ease-out ${
                   titleColorClasses[theme.color]
