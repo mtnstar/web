b1727dfaa4a1c661218b3a66a5d55380898a7d4f
Mountain Star <github@mtnstar.net>
2022-12-12 21:11:33 +1300

    Next attempt fixing github page initial page load problem

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
+import React from "react";
 import "../styles.css";
+import { useRouter } from 'next/router'
 
 const App = ({ Component, pageProps }) => {
+   const router = useRouter();
+  React.useEffect(()=>{
+        router.push(window.location.href)
+    },[])
   return <Component {...pageProps} />;
 };
 
